ee5101d0d2bad002d79a9c05ea28b4df31ed10ab
Berisoft <ariel.ameghino@gmail.com>
2024-10-26 12:15:35 -0300

    Add files via upload

---
+++ account-payment/account_payment_pro/models/res_company.py
@@ -0,0 +1,17 @@
+from odoo import api, models, fields
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    double_validation = fields.Boolean(
+        'Double Validation on Payments?',
+        help='Use two steps validation on payments to suppliers'
+    )
+    use_payment_pro = fields.Boolean(compute='_compute_use_payment_pro', store=True, readonly=False)
+
+    @api.depends('partner_id.country_id.code')
+    def _compute_use_payment_pro(self):
+        ar_companies = self.filtered(lambda x: x.partner_id.country_id.code == 'AR')
+        ar_companies.use_payment_pro = True
+        (self - ar_companies).use_payment_pro = False
