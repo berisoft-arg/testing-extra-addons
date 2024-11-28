3492590658910745f250eadeebf32308daac1904
Berisoft <ariel.ameghino@gmail.com>
2024-11-28 18:39:34 -0300

    Add files via upload

---
+++ bs-odoo-addons/product_cost_currency_usd/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Product Cost Currency USD',
+    'version': '17.0.1.0.0',
+    'summary': 'Agrega la moneda USD para precio de costo y de venta de poductos',
+    'author': 'Berisoft Gestion',
+    'website': 'https://berisoft.com.ar',
+    'license': 'AGPL-3',
+    'category': 'Product',
+    'depends': ['product'],
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
